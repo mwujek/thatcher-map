ac649bc32f6b934f1d69ee50da7cb8b9520b8cc9
Matt Wujek <mattwujek@gmail.com>
2014-08-22 17:03:58 -0700

    Friday's Work
    - Added b/w images for circles
    - placed all circles via percentages, will calibrate later
    - added most meta-data for scrolling and filters
    - cleaned up javascript…removed old code

--- scripts.js
+++ scripts.js
@@ -1,18 +1,30 @@
 var tooltip = $('#tools');
-var tipAdjustX = -47;
-var tipAdjustY = 8;
+var tipAdjustX = 25;
+var tipAdjustY = -18;
 var slideSpeed = 1000;
 //var scrollTo2 = "#scrollTo2";
+//click to add SVG File
 
-//What happens when hovering over states
-function mouseOnS(StateName, info){
-  $(tooltip).html(StateName + ' ' +info);
-  $(tooltip).css('background', 'white');
-}
-//What happens when hovering over cities
-function mouseOnC(CityName, info){
-  $(tooltip).html(CityName + ' ' +info);
-  $(tooltip).css('background', '#ddd');
+
+
+//What happens when hovering over button
+function mouseOnButton(itemLocalz, moreInfo, elementCategory){
+  $(tooltip).html('<h1>'+itemLocalz+'</h1>' + '<h2>'+moreInfo+'</h2>');
+  if(elementCategory === 'people'){
+    $(tooltip).removeClass('misc-tooltip');
+    $(tooltip).removeClass('traditions-tooltip');
+    $(tooltip).addClass('people-tooltip');
+
+  } else if (elementCategory === 'traditions'){
+    $(tooltip).removeClass('misc-tooltip');
+    $(tooltip).removeClass('people-tooltip');
+    $(tooltip).addClass('traditions-tooltip');
+  } else {
+    $(tooltip).removeClass('people-tooltip');
+    $(tooltip).removeClass('traditions-tooltip');
+    $(tooltip).addClass('misc-tooltip');
+  }
+  
 }
 
 //Last Function that tells the div to animate down towards the desire content
@@ -41,7 +53,7 @@ function openTab2(p){
 
 
 //Adds class when hovering over states & cities
-$( ".states, .cities" ).hover(
+$( ".button, .circle-cities" ).hover(
   function() {
     $(tooltip).addClass('show');
   }, function() {
@@ -49,31 +61,27 @@ $( ".states, .cities" ).hover(
   });
 
 //Updates location of Hovering Description
-$( ".states, .cities" ).mousemove(function(event) {
+$( ".button, .circle-cities" ).mousemove(function(event) {
   var currentMousePos = { x: -1, y: -1 };
   currentMousePos.x = event.pageX;
   currentMousePos.y = event.pageY;
 
   $(tooltip).css({
-    'top':(currentMousePos.y + tipAdjustX),
-    'left':(currentMousePos.x+ tipAdjustY)
+    'top':(currentMousePos.y + tipAdjustY),
+    'left':(currentMousePos.x+ tipAdjustX)
   });
 });
 
 //Simple Hover effect.
 //Uses function to change color and HTML text. 
 //See functions above
-$( ".states" ).hover(function() {
-    var stateName = $(this).data('statename');
+$( ".button, .circle-cities" ).hover(function() {
+    var menuAttr = $(this).data('element');
     var info = $(this).data('info');
-    mouseOnS(stateName, info);
-  }, function() {mouseOnS(); } );
+    var category = $(this).data('category');
+    mouseOnButton(menuAttr, info, category);
+  }, function() {mouseOnButton(); } );
 
-$( ".cities" ).hover(function() {
-     var cityName = $(this).data('cityname');
-     var info = $(this).data('info');
-     mouseOnC(cityName, info);
-}, function() {mouseOnC(); } );
 
 
 //map circle functions
@@ -119,7 +127,7 @@ function openTab3(scrollHere, category){
 
   //$('.circle-map').html('c!!!!');
   
-  $('.circle-map').on('click', function(){
+  $('.circle-map, .circle-cities').on('click', function(){
     var scrollHere = $(this).data('title');
     var category = $(this).data('category');
     openTab3(scrollHere, category);
@@ -143,63 +151,4 @@ $(function () {
     });
 
   });
-
-
-
-    $('.states').click(function(){
-      //var currentCategory = (this).data('category'); //pass this to the accordion, so it knows what to open and close
-      //var currentItem = (this).data('title'); //pass this to the openTab function, so it knows where to scroll to!
-
-      $('#places').next(".pane").siblings(".pane:visible").slideUp(slideSpeed);
-      $('#places').next(".pane").slideToggle(slideSpeed);
-      $('#places').toggleClass("currentz");
-      $('#places').siblings("h3").removeClass("currentz");
-      moveToTop();
-
-      // openTab2('places-scroll-2');
-      setTimeout(function(){openTab2('places-scroll-2');},1300);
-
-      
-
-    });
-
-    $('#links-list li').click(function(){
-
-      var currentItem = $(this); // get data below
-      var whereToScroll = currentItem.data('title');
-      var currentCategory = currentItem.data('category');
-      currentCategory = '#'+currentCategory;
-      //var currentScrollID = '#'+whereToScroll;
-
-      if($(currentCategory).hasClass('currentz')){
-
-        $(currentCategory).next().children("h1").removeClass('activeItem'); //remove active class from the entry
-        moveToTop();
-        setTimeout(function(){openTab2(whereToScroll);},1300);
-      } else{
-
-      //var totalEntries = $(currentCategory).next().children('h1').length); //get total number of entries in the current category. This is used to automate scrolling function
-      //currentEntry =  //which entry are we scrolling to?
-      //window.alert($(currentCategory).next().children('h1').index(currentItem));
-      
-      $(currentCategory).next().children("h1").removeClass('activeItem'); //remove active class from the entry
-      $(currentCategory).next(".pane").siblings(".pane:visible").slideUp(slideSpeed); // collapse all other tabls
-      setTimeout(function(){
-      $(currentCategory).next(".pane").slideToggle(slideSpeed);
-        setTimeout(function(){
-          $(currentCategory).toggleClass("currentz");
-          },100);
-      },1000); // open current slide
-      
-      $(currentCategory).siblings("h3").removeClass("currentz"); //remove any effects before the scrolling takes place
-      $(currentCategory).children("h1").removeClass('activeItem');
-      moveToTop();
-     
-
-      //setTimeout(openTab2('people-scroll-4'),2000);
-
-      setTimeout(function(){openTab2(whereToScroll);},1500);
-      }
-
-    });
 
